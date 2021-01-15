dd634f7c3f844a7cdbed31be49c0455a6f15b2d0
lixinyang123 <lixinyangemil@outlook.com>
2021-01-15 16:53:17 +0800

    hhh扩展

--- src/plugins/weather.js
+++ src/plugins/weather.js
@@ -1,3 +1,29 @@
+/*
+sender
+type:object
+=================
+age:年龄
+area:'洛阳'
+level:群等级
+nickname:'qq昵称'
+sex:'male/female'
+user_id:qq号
+*/
+
+/*
+message
+type:string
+=================
+消息内容
+*/
+
+/*
+callback
+type:function
+=================
+para:replay 回复内容
+*/
+
 const http = require("http");
 
 module.exports.exec = (sender,message,callback) => {
