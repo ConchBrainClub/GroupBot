da28286cdd7f448af2fb688b1d861442457bf3d8
lixinyang123 <lixinyangemil@outlook.com>
2021-01-15 16:21:34 +0800

    新增连续对话能力

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const {createClient} = require("oicq");
 const fs = require("fs");
 
 let info = JSON.parse(fs.readFileSync("config.json"));
+let currentUser = new Array();
 
 const bot = createClient(info.account);
 
@@ -17,27 +18,53 @@ bot.on("message", data => {
     return;
   }
 
-  let pattern = /\[(.*)]/;
-  let info = pattern.exec(data.raw_message)[0];
-
-  if(!info.includes("@ConchBrainBot")){
+  if(currentUser.includes(data.sender.user_id)){
+    handlerMessage(data);
     return;
   }
 
+  let pattern = /\[(.*)]/;
+  let info = pattern.exec(data.raw_message)[0];
+  if(info.includes("@ConchBrainBot")){
+    handlerMessage(data);
+  }
+
+});
+
+function handlerMessage(data){
   console.log(data);
+  if(!currentUser.includes(data.sender.user_id))
+    currentUser.push(data.sender.user_id);
 
   let message = data.raw_message.replace(info,"").trim();
+
+  if(new RegExp("再见|拜拜").test(message)){
+    let index = currentUser.indexOf(data.sender.user_id);
+    currentUser.splice(index,1);
+    bot.sendGroupMsg(data.group_id, "拜拜" + data.sender.nickname);
+    return;
+  }
 
   let pluginConfigs = JSON.parse(fs.readFileSync("./src/pattern.json").toString());
   pluginConfigs.forEach((config) => {
 
     if(new RegExp(config.pattern).test(message)){
-      require("./plugins/" + config.plugin).exec(data.sender, message, (replay) => {
+
+      let plugin = require("./plugins/" + config.plugin);
+
+      if(!plugin){
+        let index = pluginConfigs.indexOf(config.plugin);
+        pluginConfigs.splice(index,1);
+        fs.writeFileSync("./src/pattern.json", JSON.stringify(pluginConfigs));
+        return;
+      }
+
+      plugin.exec(data.sender, message, (replay) => {
         bot.sendGroupMsg(data.group_id, replay);
-      })
+      });
+
     }
   });
-
-});
+}
 
 bot.login(info.password);
